ce228f64ffdd0601b9d78e629cfdbb6da3a5652a
mohamed-sakr99 <mohsakr9191@gmail.com>
2022-02-26 16:32:40 +0200

    last

--- src/app/services/dashboard/drop-down-menus.service.ts
+++ src/app/services/dashboard/drop-down-menus.service.ts
@@ -1,12 +1,12 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DropDownMenusService {
-  private apiServer = 'http://mkarim225-001-site1.ftempurl.com/api/';
+  private apiServer = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
   getCategoryLookups() {
